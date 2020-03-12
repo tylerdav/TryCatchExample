014c4ade4fdeb89abcd429effb98316e95e066d9
tylerdav <tdavis1433@att.net>
2020-03-12 10:54:34 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TryCatchIntro
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int difficulty = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write ("What is the bank difficulty? ");
+                    var userInput = Console.ReadLine ();
+                    difficulty = int.Parse (userInput);
+                    var taco = 100 / difficulty;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine ("Oops something went wrong");
+                }
+            }
+
+            Console.WriteLine ($"The difficulty is {difficulty}");
+        }
+    }
+}
